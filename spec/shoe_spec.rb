3cbca505ddada56f2aaebcf36f9a5311d7cb31bf
Mark <heltmm@gmail.com>
2017-09-29 16:51:12 -0700

    validation specs for all classes passing

--- spec/shoe_spec.rb
+++ spec/shoe_spec.rb
@@ -2,5 +2,23 @@ require('spec_helper')
 
 describe(Shoe) do
   it { should have_many(:products)}
+
   it { should have_many(:stores)}
+
+  it("validates presence of brand") do
+    shoe = Shoe.new({:brand => ''})
+    expect(shoe.save()).to(eq(false))
+  end
+
+  it("validates presence of price") do
+    shoe = Shoe.new({:price => ''})
+    expect(shoe.save()).to(eq(false))
+  end
+
+  it { should validate_length_of(:brand).is_at_most(100) }
+
+  it('capitalizes the first letter of every word in brand') do
+    shoe = Shoe.create({:brand => "air jordans", :price => "44"})
+    expect(shoe.brand).to eq("Air Jordans")
+  end
 end
--- spec/store_spec.rb
+++ spec/store_spec.rb
@@ -2,5 +2,23 @@ require('spec_helper')
 
 describe(Store) do
   it { should have_many(:products)}
+
   it { should have_many(:shoes)}
+
+  it("validates presence of name") do
+    store = Store.new({:name => ''})
+    expect(store.save()).to(eq(false))
+  end
+
+  it("validates presence of location") do
+    store = Store.new({:location => ''})
+    expect(store.save()).to(eq(false))
+  end
+
+  it { should validate_length_of(:name).is_at_most(100) }
+
+  it('capitalizes the first letter of every word in name') do
+    store = Store.create({:name => "jim's discount shoe store", :location => "mars"})
+    expect(store.name).to eq("Jim's Discount Shoe Store")
+  end
 end
