96c242d718f18283ffd071ed117278c2432acb92
Mark <heltmm@gmail.com>
2017-09-29 11:25:27 -0700

    create alert system if store is saved or not

--- app.rb
+++ app.rb
@@ -4,5 +4,23 @@ Bundler.require(:default)
 Dir[File.dirname(__FILE__) + '/lib/*.rb'].each { |file| require file }
 
 get('/') do
+  @stores = Store.all
+  @shoes = Shoe.all
   erb(:index)
+end
+
+post('/create/store') do
+  @store = Store.create({:name => params['name'], :location => params['location']})
+  if @store.id
+    @saved = true
+  else
+    @not_saved = true
+  end
+  @stores = Store.all
+  @shoes = Shoe.all
+  erb(:index)
+end
+
+post('/create/shoe') do
+
 end
